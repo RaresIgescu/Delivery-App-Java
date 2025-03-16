c9a7056acd1c9b8968827dbc6aa9d1e63a7de076
Rares Igescu <raresigescu@gmail.com>
2025-03-16 18:12:17 +0200

    Update curier class with rating attribute

--- Curier.java
+++ Curier.java
@@ -3,12 +3,14 @@ public class Curier {
     private String nume;
     private String prenume;
     private String nationalitate;
+    private float rating;
 
-    public Curier(int id, String nume, String prenume, String nationalitate) {
+    public Curier(int id, String nume, String prenume, String nationalitate, float rating) {
         this.id = id;
         this.nume = nume;
         this.prenume = prenume;
         this.nationalitate = nationalitate;
+        this.rating = rating;
     }
 
     public int getId() {
@@ -37,5 +39,12 @@ public class Curier {
     }
     public void setNationalitate(String nationalitate) {
         this.nationalitate = nationalitate;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+    public void setRating(float rating) {
+        this.rating = rating;
     }
 }
