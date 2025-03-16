922c084825cea9d435baa9d87bb40c4574ff1eb4
Rares Igescu <raresigescu@gmail.com>
2025-03-16 16:54:17 +0200

    First commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Interogari:");
+        System.out.println("1. Vizualizare date personale.");
+        System.out.println("2. Vizualizare restaurante.");
+        System.out.println("3. Vizualizare meniu.");
+        System.out.println("4. Vizualizare foste comenzi.");
+        System.out.println("5. Vizualizare cos de cumparaturi.");
+        System.out.println("6. Vizualizare carduri de credit salvate.");
+        System.out.println("7. Filtrare restaurante dupa tip.");
+
+        System.out.println("Actiuni:");
+        System.out.println("8. Creare cont.");
+        System.out.println("9. Login/Logout/");
+        System.out.println("10. Adaugare produs in cos.");
+        System.out.println("11. Plasare comanda.");
+        System.out.println("12. Modificare adresa comanda.");
+        System.out.println("13. Adaugare review.");
+        System.out.println("14. Editare review.");
+        System.out.println("15. Sterge Review.");
+        System.out.println("16. Adaugare card de credit.");
+        System.out.println("17. Sterge card de credit.");
+        System.out.println("18. Anulare comanda.");
+    }
+}
