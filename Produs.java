1c9c9fd3a6a83d731161e7f9a337d91e7df30502
Rares Igescu <raresigescu@gmail.com>
2025-03-16 19:01:33 +0200

    Create and define Produs class

---
+++ Produs.java
@@ -0,0 +1,45 @@
+public class Produs {
+    private int id;
+    private String nume;
+    private float pret;
+    private String disponibilitate;
+
+    public Produs(int id, String nume, float pret, String disponibilitate) {
+        this.id = id;
+        this.nume = nume;
+        this.pret = pret;
+        this.disponibilitate = disponibilitate;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public float getPret() {
+        return pret;
+    }
+    public void setPret(float pret) {
+        this.pret = pret;
+    }
+
+    public String getDisponibilitate() {
+        return disponibilitate;
+    }
+    public void setDisponibilitate(String disponibilitate) {
+        this.disponibilitate = disponibilitate;
+    }
+
+    public String toString() {
+        return "Nume: " + nume + "Pret: " + pret + "Disponibilitate: " + disponibilitate;
+    }
+}
