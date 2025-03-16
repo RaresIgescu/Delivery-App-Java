432721b4574a5fa81423d84c5ca96d7cba14bd74
Rares Igescu <raresigescu@gmail.com>
2025-03-16 18:17:37 +0200

    Fully define user class

--- User.java
+++ User.java
@@ -1,2 +1,60 @@
 public class User {
+    private int id;
+    private String nume;
+    private String prenume;
+    private int varsta;
+    private String oras;
+    private String strada;
+
+    public User(int id, String nume, String prenume, int varsta, String oras, String strada) {
+        this.id = id;
+        this.nume = nume;
+        this.prenume = prenume;
+        this.varsta = varsta;
+        this.oras = oras;
+        this.strada = strada;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public String getPrenume() {
+        return prenume;
+    }
+    public void setPrenume(String prenume) {
+        this.prenume = prenume;
+    }
+
+    public int getVarsta() {
+        return varsta;
+    }
+    public void setVarsta(int varsta) {
+        this.varsta = varsta;
+    }
+
+    public String getOras() {
+        return oras;
+    }
+    public void setOras(String oras) {
+        this.oras = oras;
+    }
+
+    public String getStrada() {
+        return strada;
+    }
+    public void setStrada(String strada) {
+        this.strada = strada;
+    }
+
 }
