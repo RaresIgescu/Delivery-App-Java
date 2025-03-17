654e417a19c5bf56d9732e2b3a98e153c7ba3623
Rares Igescu <raresigescu@gmail.com>
2025-03-17 11:27:32 +0200

    Define switch statement

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -27,6 +26,70 @@ public class Main {
             System.out.println("16. Adaugare card de credit.");
             System.out.println("17. Sterge card de credit.");
             System.out.println("18. Anulare comanda.");
+            System.out.println("0. Iesire");
+
+            System.out.println("Alege o optiune: ");
+            int optiune = scan.nextInt();
+
+            switch(optiune) {
+                case 1:
+                    System.out.println("Vizualizare date personale.");
+                    break;
+                case 2:
+                    System.out.println("Vizualizare restaurante.");
+                    break;
+                case 3:
+                    System.out.println("Vizualizare meniu.");
+                    break;
+                case 4:
+                    System.out.println("Vizualizare foste comenzi.");
+                    break;
+                case 5:
+                    System.out.println("Vizualizare cos de cumparaturi.");
+                    break;
+                case 6:
+                    System.out.println("Vizualizare carduri de credit.");
+                    break;
+                case 7:
+                    System.out.println("Filtrare restaurante dupa tip.");
+                    break;
+                case 8:
+                    System.out.println("Creare cont");
+                    break;
+                case 9:
+                    System.out.println("Login/Logout");
+                    break;
+                case 10:
+                    System.out.println("Adaugare produs in cos.");
+                    break;
+                case 11:
+                    System.out.println("Plasare comanda.");
+                    break;
+                case 12:
+                    System.out.println("Modificare adresa comanda.");
+                    break;
+                case 13:
+                    System.out.println("Adaugare review.");
+                    break;
+                case 14:
+                    System.out.println("Editare review.");
+                    break;
+                case 15:
+                    System.out.println("Sterge Review.");
+                    break;
+                case 16:
+                    System.out.println("Adaugare card de credit.");
+                    break;
+                case 17:
+                    System.out.println("Sterge card de credit.");
+                    break;
+                case 0:
+                    System.out.println("Iesire...");
+                    ok = false;
+                    break;
+                default:
+                    System.out.println("Optiune invalida");
+            }
         }
     }
 }
