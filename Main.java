b0f3c543327cc7d2b90eec6a57d1fa43ecffe708
Rares Igescu <raresigescu@gmail.com>
2025-03-26 18:42:18 +0200

    Changed print style

--- Main.java
+++ Main.java
@@ -5,22 +5,30 @@ public class Main {
         Service service = new Service();
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Bune aveti veni pe placeIT, cea mai noua platforma de food delivery!");
+        System.out.println("===================================================");
+        System.out.println("             Bine ati venit pe placeIT!");
+        System.out.println("      Cea mai noua platforma de food delivery");
+        System.out.println("===================================================");
+        System.out.println();
         System.out.println("Mai intai de toate va rog sa va introduceti datele personale: ");
         service.setareDatePersonale();
 
-        //comment
-        //comment 2
         boolean ok = true;
-        while(ok) {
-            System.out.println("Interogari:");
+        while (ok) {
+            System.out.println();
+            System.out.println("====================================");
+            System.out.println("            Interogari:");
+            System.out.println("====================================");
             System.out.println("1. Vizualizare date personale.");
             System.out.println("2. Vizualizare meniu.");
             System.out.println("3. Vizualizare foste comenzi.");
             System.out.println("4. Vizualizare cos de cumparaturi.");
             System.out.println("5. Filtrare restaurante dupa tip.");
             System.out.println();
-            System.out.println("Actiuni:");
+
+            System.out.println("====================================");
+            System.out.println("            Actiuni:");
+            System.out.println("====================================");
             System.out.println("6. Modificare date personale.");
             System.out.println("7. Adaugare produs in cos.");
             System.out.println("8. Plasare comanda.");
@@ -28,37 +36,38 @@ public class Main {
             System.out.println("10. Adaugare review pentru un restaurant.");
             System.out.println("11. Adaugare review pentru curier.");
             System.out.println("12. Sterge o comanda din memorie.");
+            System.out.println();
+            System.out.println("====================================");
             System.out.println("0. Iesire");
+            System.out.println("====================================");
 
-            System.out.println("Alege o optiune: ");
+            System.out.print("Alege o optiune: ");
             int optiune = scan.nextInt();
             scan.nextLine();
 
             switch(optiune) {
-                case 1: //VERIFICAT COMPLET
-                    System.out.println("Acestea sunt datele dumneavoastra personale: ");
+                case 1:
                     service.vizualzareDatePersonale();
                     break;
-                case 2: //VERIFICAT COMPLET
+                case 2:
                     service.vizualizareMeniu();
                     break;
-                case 3: //VERIFICAT COMPLET - De aduaugat o afisare mai frumoasa
-                        //De vazut de bug ca se adauga si la vechile comenzi noile produse
+                case 3:
                     service.vizualizareFosteComenzi();
                     break;
-                case 4: //Verificat complet - De golit cosul cand plasez o comanda
+                case 4:
                     service.vizualizareCos();
                     break;
-                case 5: //VERIFICAT COMPLET
+                case 5:
                     service.filtrareRestaurante();
                     break;
-                case 6: //VERIFICAT COMPLET
+                case 6:
                     service.setareDatePersonale();
                     break;
-                case 7: //VERIFICAT COMPLET - De golit cos dupa ce plasam comanda
+                case 7:
                     service.adaugareProdusInCos();
                     break;
-                case 8: //DE GOLIT COS!!!!!!!!!!!!!!!!!!!!!!!!!!
+                case 8:
                     service.plasareComanda();
                     break;
                 case 9:
