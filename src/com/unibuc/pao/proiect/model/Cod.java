d09a9c1cc2f71345157edee1e6d3358ac2264caa
Rares Igescu <raresigescu@gmail.com>
2025-06-02 15:40:36 +0300

    Define setters and getters for cod class

--- src/com/unibuc/pao/proiect/model/Cod.java
+++ src/com/unibuc/pao/proiect/model/Cod.java
@@ -1,4 +1,43 @@
 package src.com.unibuc.pao.proiect.model;
 
-public class Coduri {
+import java.sql.SQLOutput;
+
+public class Cod {
+    private int id;
+    private String cod;
+    private String valabilitate;
+
+    public Cod(int id, String cod, String valabilitate) {
+        this.id = id;
+        this.cod = cod;
+        this.valabilitate = valabilitate;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getCod() {
+        return cod;
+    }
+
+    public String getValabilitate() {
+        return valabilitate;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+
+    public void setValabilitate(String valabilitate) {
+        this.valabilitate = valabilitate;
+    }
+
+    @Override
+    public String toString() {
+        return "/===== COD =====/\nCod promotional: " + cod + "\nValabilitate: " + valabilitate + "\n";
+    }
 }
